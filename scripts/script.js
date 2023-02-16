a7b34fbb01b26802c9279d9a2324da74b7185c16
Nolan Knill <nolanknill@gmail.com>
2023-02-16 15:35:36 -0500

    Adds showing contestants from a promise, including error callback handling

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,46 @@
-getContestants().then((result) => {
-  console.log(result);
-})
+/*
+let promise = asyncPromiseFunc();
+promise.then(
+  (result) => {
+    console.log(result);
+  },
+  (errorObject) => {
+    console.error(errorObject.error);
+  }
+)
+*/
+
+
+let promise = getContestants();
+
+promise.then(
+  (result) => {
+    console.log(result);
+
+    // 1. Remove the loading under contestants
+    document.querySelector(".contestants__loading").remove();
+    
+    // 2. Render the contestants to the DOM
+    const listEl = document.querySelector(".contestants__list");
+    result.forEach((contestant) => {
+      // 3. Add contestant name as an li inside the ".contestants__list" element
+      const itemEl = document.createElement("li");
+      itemEl.innerText = contestant.name;
+      listEl.appendChild(itemEl);
+    })
+
+  },
+  (errorObject) => {
+    // Error message as string in errorObject.error
+    console.error(errorObject.error);
+    
+    document.querySelector(".contestants__loading").remove();
+    const contestantsEl = document.querySelector(".contestants");
+
+    const errorEl = document.createElement("p");
+    errorEl.innerText = errorObject.error + ". Try refreshing the page";
+
+    contestantsEl.appendChild(errorEl);
+
+  }
+)
